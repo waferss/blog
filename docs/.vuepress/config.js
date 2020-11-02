5ecf16acc1117c1ea8f904d6b22786bdf494a56d
Norman <37663295+chickenAndFish@users.noreply.github.com>
2020-11-02 08:52:39 +0800

    blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,46 @@
+/** @format */
+
+module.exports = {
+  base: '/blog/',
+  title: 'Blog of chickenAndFish',
+  description: "It's just a blog",
+  keys: '个人博客',
+  repo: 'https://github.com/chickenAndFish/blog.git',
+  head: [['link', { rel: 'shortcut icon', href: '/favicon.ico' }]],
+  markdown: {
+    lineNumbers: true, // 代码块是否加行号
+  },
+  // webpack路径别名配置
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@assets': '/docs/assets',
+      },
+    },
+  },
+  // 主题配置
+  themeConfig: {
+    logo: '/CAF.png',
+    nav: [
+      { text: '首页', link: '/', ariaLabel: '首页' },
+      { text: '文章', link: '/guide/', ariaLabel: '文章' },
+      {
+        text: '了解更多',
+        ariaLabel: '了解更多',
+        items: [
+          { text: 'jiandyu', link: '/' },
+          { text: 'more', link: '/' },
+        ],
+      },
+      {
+        text: 'Github',
+        ariaLabel: 'Github',
+        link: 'https://github.com/chickenAndFish',
+      },
+    ],
+  },
+  sidebar: 'auto', // 侧边栏配置
+  sidebarDepth: 2, // 侧边栏显示2级
+  lastUpdated: 'Last Updated', // 最后更新时间
+  smoothScroll: true, // 页面滚动
+};
---
+++ docs/README.md
@@ -0,0 +1,13 @@
+---
+home: true
+# heroImage: /assets/hero.png
+heroText: 一个简单的博客
+tagline: '@chickenAndFish'
+actionText: 查看文章 →
+actionLink: /guide/
+features:
+- title: 不复杂
+  details: 简单不在乎
+footer: 这里是底部 | Copyright © 2020-present chickenAndFish
+---
+# Hello World
